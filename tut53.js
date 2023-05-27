19d955c6f071a8debfb263a893a7947d4a0a3c76
shravan <kumashravan5@gmail.com>
2023-05-27 10:51:31 +0530

    learnt about prompt , alerts and confirms injava script

--- tut53.js
+++ tut53.js
@@ -1,1 +1,13 @@
-console.log('this is tutorial 53')
+function greet(name ,greentext="the gay is "){
+    console.log(greentext +" "+ name)
+    console.log(name+" is a good boy")
+}
+let name1="harry"
+let name2="barry"
+let name3="tarry"
+let name4="carry"
+let greentext="goodmorning"
+greet (name1,greentext)
+greet (name2,greentext)
+greet (name3,greentext)
+greet(name4,)
